472b3da5ae2caf5b57aee8b0548e8b866084d3cc
Arthur Tran <tran.art13@gmail.com>
2019-04-21 03:25:57 -0700

    Added functionality for going through directories

--- lineCounter.py
+++ lineCounter.py
@@ -1,29 +1,48 @@
 import os
 
-def validExt(ext):
+def validExt(filename):
 
     validExtentions = [".java", ".py", ".cpp", ".h"]
 
-    file = input("Enter a filename: ")
-
     index = 0
-    while file[index : index + 1] != "." and index != len(file):
+    while filename[index : index + 1] != "." and index != len(filename):
         index += 1
 
-    return (file[index : len(file)] in validExtentions)
+    return (filename[index : len(filename)] in validExtentions)
+
+def countLines(dir):
+    counter = 0
+
+    file = open(dir, errors = 'ignore').readlines()
+
+    for _ in file:
+        counter += 1
+
+    return counter
+
+def crawlDirectories(dir, total):
+    for filename in os.listdir(dir):
+
+        if os.path.isdir(dir + "\\" + filename): 
+            crawlDirectories(dir + "\\" + filename, total)
+
+        else:
+            if(validExt(filename)):
+                numLines = countLines(dir + "/" + filename) 
+                print(filename + ": " + str(numLines) + " lines")
+                total[0] = total[0] + numLines
+
+def countTotalLines(dir):
+    total = [0]  
+    crawlDirectories(dir, total)
+
+    return total[0]     
 
 def main():
 
-    counter = 0
     directory = input("Enter directory of files: ")
 
-    for filename in os.listdir(directory):
-        file = open(directory + "\\" + filename, "r").readlines()
-
-        for _ in file:
-            counter += 1
-
-    print(counter)
+    print("\nTotal: " + str(countTotalLines(directory)))
 
 if __name__ == "__main__":
     main()
